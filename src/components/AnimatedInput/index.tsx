f9bb3b02fc10e2be743a6f611712eb79f1ecdeb7
me <bhavesh.pareek36@gmail.com>
2020-11-19 00:19:48 +0530

    simplify animatedInput

--- src/components/AnimatedInput/index.tsx
+++ src/components/AnimatedInput/index.tsx
@@ -1,12 +1,9 @@
 import {
+  ComponentChild,
   RefType,
   VNode,
   useCallback,
-  useEffect,
   useMemo,
-  useState,
-  ComponentChild,
-  useRef,
 } from "@hydrophobefireman/ui-lib";
 import {
   errorCss,
@@ -18,15 +15,15 @@ import {
 interface InputProps
   extends Omit<JSX.HTMLAttributes<HTMLInputElement>, "onInput" | "icon"> {
   id?: string;
-  onInput(value: string): void;
   value?: string;
   wrapperClass?: string;
   labelText?: string;
   inputClass?: string | string[];
   inputProps?: JSX.HTMLAttributes;
   errorText?: string;
-  $ref?: RefType<HTMLInputElement>;
+  ref?: RefType<HTMLInputElement>;
   icon?: ComponentChild;
+  onInput(value: string): void;
 }
 
 export function AnimatedInput(props: InputProps): VNode {
@@ -34,50 +31,30 @@ export function AnimatedInput(props: InputProps): VNode {
 
   const {
     id = randomId,
-    onInput: propOnInput,
     wrapperClass,
+    onInput,
     labelText,
     inputClass,
     errorText,
-    $ref,
     icon,
+    value,
     ...rest
   } = props;
-
-  const [value, setValue] = useState(props.value || "");
-  const ref = useRef<HTMLInputElement>();
-  const selectionRange = useRef<[number, number]>([0, 0]);
-  $ref && ($ref.current = ref.current);
-  useEffect(() => {
-    setValue(props.value);
-  }, [props.value]);
-  useEffect(() => {
-    const { current } = ref;
-    if (!current) return;
-    const [s, e] = selectionRange.current;
-    current.setSelectionRange(s, e);
-  }, [selectionRange.current]);
-  function handleInput(e: JSX.TargetedKeyboardEvent<HTMLInputElement>): void {
-    const val = (e.target as HTMLInputElement).value;
-    setValue(val);
-    const { current } = ref;
-    current &&
-      (selectionRange.current = [current.selectionStart, current.selectionEnd]);
-    propOnInput && propOnInput(val);
-  }
-  //   active || errorText ? moveUp : moveDown
-  const onInput = useCallback(handleInput, [propOnInput]);
-
+  const handleInput = useCallback(
+    function (e: JSX.TargetedKeyboardEvent<HTMLInputElement>) {
+      return onInput(e.currentTarget.value);
+    },
+    [onInput]
+  );
   return (
     <div class={[wrapperCSS].concat(wrapperClass)}>
       {icon && <span class={iconCSS}>{icon}</span>}
       <input
-        onInput={onInput}
+        onInput={handleInput}
         id={id}
         value={value}
         data-error={!!errorText}
         class={[paperInput].concat(inputClass)}
-        ref={ref}
         data-should-focus={!!value}
         {...rest}
       />
